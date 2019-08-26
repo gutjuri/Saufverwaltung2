c39b424f0c982b292a51a558da111086011a7d2c
Juri Dispan <juri.dispan@uni-ulm.de>
2019-08-26 12:42:58 +0200

    fixed bug where window would freeze on file open

--- src/main/java/saufverwaltung/util/ListFileHandler.java
+++ src/main/java/saufverwaltung/util/ListFileHandler.java
@@ -10,100 +10,105 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class ListFileHandler {
-    private final String path;
-    private final Localizer msg;
+	private final String path;
+	private final Localizer msg;
 
-    private final List<Integer> colWidths = Arrays.asList(9, 27, 27, 9);
-    private final String lineSeparator = times("-", colWidths.get(0)) + "+"
-                    + times("-", colWidths.get(1) + 1) + "+" + times("-", colWidths.get(2) + 1)
-                    + "+" + times("-", colWidths.get(3));
+	private final List<Integer> colWidths = Arrays.asList(9, 27, 27, 9);
+	private final String lineSeparator = times("-", colWidths.get(0)) + "+" + times("-", colWidths.get(1) + 1) + "+"
+			+ times("-", colWidths.get(2) + 1) + "+" + times("-", colWidths.get(3));
 
+	public ListFileHandler(String path, Localizer msg) {
+		this.path = path;
+		this.msg = msg;
+	}
 
-    public ListFileHandler(String path, Localizer msg) {
-        this.path = path;
-        this.msg = msg;
-    }
+	public void writeListToFile(List<Member> memberlist) {
+		Date date = Calendar.getInstance().getTime(); // Date
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+		String dateString = dateFormatter.format(date);
+		try {
+			BufferedWriter w = new BufferedWriter(new FileWriter(path));
+			String listname = msg.getString("list");
+			String boozename = msg.getString("booze");
+			String nbname = msg.getString("nonalcoholics");
+			String blocked = msg.getString("blocked");
+			w.write(padRight(listname, colWidths.stream().reduce(0, (a, b) -> a + b) - dateString.length() + 5)
+					+ dateString);
+			w.newLine();
+			w.newLine();
+			w.write(padRight(msg.getString("name") + ":", colWidths.get(0)) + "| "
+					+ padRight(boozename + " (1,50\u20ac)", colWidths.get(1)) + "| "
+					+ padRight(nbname + " (1\u20ac)", colWidths.get(2)) + "| "
+					+ padRight(msg.getString("cap"), colWidths.get(3)));
+			w.newLine();
+			w.write(lineSeparator);
+			w.newLine();
+			for (Member cm : memberlist) {
+				if (!cm.isVisible()) {
+					continue;
+				}
 
-    public void writeListToFile(List<Member> memberlist) {
-        Date date = Calendar.getInstance().getTime(); // Date
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
-        String dateString = dateFormatter.format(date);
-        try {
-            BufferedWriter w = new BufferedWriter(new FileWriter(path));
-            String listname = msg.getString("list");
-            String boozename = msg.getString("booze");
-            String nbname = msg.getString("nonalcoholics");
-            String blocked = msg.getString("blocked");
-            w.write(padRight(listname,
-                            colWidths.stream().reduce(0, (a, b) -> a + b) - dateString.length() + 5)
-                            + dateString);
-            w.newLine();
-            w.newLine();
-            w.write(padRight(msg.getString("name") + ":", colWidths.get(0)) + "| "
-                            + padRight(boozename + " (1,50\u20ac)", colWidths.get(1)) + "| "
-                            + padRight(nbname + " (1\u20ac)", colWidths.get(2)) + "| "
-                            + padRight(msg.getString("cap"), colWidths.get(3)));
-            w.newLine();
-            w.write(lineSeparator);
-            w.newLine();
-            for (Member cm : memberlist) {
-                if (!cm.isVisible()) {
-                    continue;
-                }
+				String blockedStr = padRight(cm.getGuthaben() < 0 ? blocked : " ", colWidths.get(1));
+				w.write(padRight(cm.getName(), colWidths.get(0)) + "| " + blockedStr + "| " + blockedStr + "|"
+						+ padLeft(cm.getGuthabenFormatted() + "\u20ac", colWidths.get(3)));
 
-                String blockedStr =
-                                padRight(cm.getGuthaben() < 0 ? blocked : " ", colWidths.get(1));
-                w.write(padRight(cm.getName(), colWidths.get(0)) + "| " + blockedStr + "| "
-                                + blockedStr + "|"
-                                + padLeft(cm.getGuthabenFormatted() + "\u20ac", colWidths.get(3)));
+				w.newLine();
+				w.write(lineSeparator);
+				w.newLine();
 
-                w.newLine();
-                w.write(lineSeparator);
-                w.newLine();
+			}
+			w.flush();
+			w.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-            }
-            w.flush();
-            w.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+	public void openFile() {
+		// System.out.println("called");
+		if (!Desktop.isDesktopSupported()) {
+			// System.out.println("nosup");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle(msg.getString("error"));
+			alert.setContentText(msg.getString("notsup") + ".");
+			alert.showAndWait();
+			return;
+		}
+		// System.out.println("sup");
+		Desktop desktop = Desktop.getDesktop();
+		File file = new File(path);
+		new Thread(() -> {
+			try {
+				desktop.open(file);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			}
+		}).start();
+	}
 
-    public void openFile() {
-        if (!Desktop.isDesktopSupported()) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle(msg.getString("error"));
-            alert.setContentText(msg.getString("notsup") + ".");
-            alert.showAndWait();
-        }
-        Desktop desktop = Desktop.getDesktop();
-        File file = new File(path);
-        try {
-            desktop.open(file);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+	private String padRight(String s, int n) {
+		return String.format("%1$-" + n + "s", s);
+	}
 
-    private String padRight(String s, int n) {
-        return String.format("%1$-" + n + "s", s);
-    }
+	private static String padLeft(String s, int n) {
+		return String.format("%1$" + n + "s", s);
+	}
 
-    private static String padLeft(String s, int n) {
-        return String.format("%1$" + n + "s", s);
-    }
+	private String times(String s, int n) {
+		StringBuilder sb = new StringBuilder();
 
-    private String times(String s, int n) {
-        StringBuilder sb = new StringBuilder();
+		while (n-- > 0) {
+			sb.append(s);
+		}
 
-        while (n-- > 0) {
-            sb.append(s);
-        }
-
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 }
